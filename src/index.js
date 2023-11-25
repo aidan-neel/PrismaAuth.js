9a04a2ce775b1d99bc63ab013fd8e79c8c1da410
ShockPlease <aaidan5899@gmail.com>
2023-11-25 04:12:24 -0600

    First commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Adds two numbers.
+ * @param {number} a The first number.
+ * @param {number} b The second number.
+ * @return {number} The sum of the two numbers.
+ */
+function add(a, b) {
+    return a + b;
+}
+
