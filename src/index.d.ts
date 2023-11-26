b102d8c9369f736b8b48c4c98ade8d901fb2b504
ShockPlease <aaidan5899@gmail.com>
2023-11-25 19:29:36 -0600

    General updates?

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,41 +1,42 @@
-import { PrismaClient } from '@prisma/client';
+declare module '@aaidan5899/prisma-auth' {
+    import { PrismaClient } from '@prisma/client';
+    export class PrismaAuth {
+        constructor(prisma: PrismaClient);
 
-export class PrismaAuth {
-    constructor(prisma: PrismaClient);
+        cleanupExpiredSessions(): Promise<void>;
 
-    cleanupExpiredSessions(): Promise<void>;
+        loginUser(email: string, plainTextPassword: string): Promise<LoginData>;
 
-    loginUser(email: string, plainTextPassword: string): Promise<LoginData>;
+        registerUser(email: string, plainTextPassword: string, name: string): Promise<SessionData>;
 
-    registerUser(email: string, plainTextPassword: string, name: string): Promise<SessionData>;
+        createSessionCookie(session: Session): string;
+    }
 
-    createSessionCookie(session: Session): string;
-}
+    // Define additional types as used in your class
+    export interface LoginData {
+        token: string;
+        session: Session;
+        user: User;
+    }
 
-// Define additional types as used in your class
-export interface LoginData {
-    token: string;
-    session: Session;
-    user: User;
-}
+    export interface SessionData {
+        token: string;
+        session: Session;
+    }
 
-export interface SessionData {
-    token: string;
-    session: Session;
-}
+    export interface Session {
+        id: number;
+        token: string;
+        createdAt: Date;
+        expiresAt: Date;
+        invalidated: boolean;
+        user: User;
+    }
 
-export interface Session {
-    id: number;
-    token: string;
-    createdAt: Date;
-    expiresAt: Date;
-    invalidated: boolean;
-    user: User;
-}
-
-export interface User {
-    id: number;
-    email: string;
-    hashedPassword: string;
-    name: string;
+    export interface User {
+        id: number;
+        email: string;
+        hashedPassword: string;
+        name: string;
+    }
 }
