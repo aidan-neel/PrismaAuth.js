c8e2eb534588d03969d79ce8b8a68077a0a5eab5
ShockPlease <aaidan5899@gmail.com>
2023-11-27 20:23:22 -0600

    deploy

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,54 +1,66 @@
-import { PrismaClient } from '@prisma/client';
-import { EventEmitter } from 'events';
 
 declare module '@aaidan5899/prisma-auth' {
-
-  // Define any custom types used in your class, like sessionData, loginData, etc.
-  type session = {
-    id: number;
-    token: string;
-    createdAt: Date;
-    expiresAt: Date;
-    invalidated: boolean;
-    user: user;
-  };
-
-  type sessionData = {
-    token: string;
-    session: session;
-    user: user;
-  };
-
-  type loginData = {
-    token: string;
-    session: session;
-    user: user;
-  };
-
-  type user = {
-    id: number;
-    email: string;
-    hashedPassword: string;
-    name: string;
-  };
-
-  // Declare the PrismaAuth class
+  import { PrismaClient, Session, User } from '@prisma/client'; // Assuming PrismaClient, User, Session types are imported from '@prisma/client'
+  import { EventEmitter } from 'events';
+  
   export class PrismaAuth extends EventEmitter {
-    currentUser: user | null;
-
-    constructor(prisma: PrismaClient);
-
-    validateSession(sessionToken: string): Promise<boolean>;
-    getSessionData(sessionToken: string): Promise<session | null>;
-    cleanupExpiredSessions(): Promise<void>;
-    loginUser(email: string, plainTextPassword: string): Promise<loginData>;
-    validatePassword(plainTextPassword: string, hashedPassword: string): Promise<boolean>;
-    registerUser(email: string, plainTextPassword: string, name: string): Promise<sessionData>;
-    userExists(email: string): Promise<boolean>;
-    createSession(userId: number, token: string, expiration?: number): Promise<session>;
-    createUser(email: string, hashedPassword: string, name: string): Promise<user>;
-    hashPassword(plainTextPassword: string): Promise<string>;
-    passwordStrengthChecker(password: string): string;
-    createSessionCookie(session: session): string;
+      currentUser: User | null;
+  
+      constructor(prisma: PrismaClient);
+  
+      validateSession(sessionToken: string): Promise<boolean>;
+  
+      signUserOut(sessionToken: string): Promise<boolean>;
+  
+      getSessionData(sessionToken: string): Promise<Session | null>;
+  
+      cleanupExpiredSessions(): Promise<void>;
+  
+      loginUser(email: string, plainTextPassword: string): Promise<LoginData>;
+  
+      validatePassword(plainTextPassword: string, hashedPassword: string): Promise<boolean>;
+  
+      registerUser(email: string, plainTextPassword: string, name: string): Promise<SessionData>;
+  
+      userExists(email: string): Promise<boolean>;
+  
+      createSession(userId: number, token: string, expiration?: number): Promise<Session>;
+  
+      createUser(email: string, hashedPassword: string, name: string): Promise<User>;
+  
+      hashPassword(plainTextPassword: string): Promise<string>;
+  
+      passwordStrengthChecker(password: string): string;
+  
+      createSessionCookie(session: Session): string;
   }
+  
+  export interface LoginData {
+      token: string;
+      session: Session;
+      user: User;
+  }
+  
+  export interface SessionData {
+      token: string;
+      session: Session;
+      user: User;
+  }
+  
+  export interface Session {
+      id: number;
+      jwtToken: string;
+      createdAt: Date;
+      expiresAt: Date;
+      invalidated: boolean;
+      user: User;
+  }
+  
+  export interface User {
+      id: number;
+      email: string;
+      password: string;
+      name: string;
+  }
+  
 }
